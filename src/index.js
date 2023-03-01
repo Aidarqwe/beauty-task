930b7eddb659664cf90a2a7ae2c445098c1b84da
Aidarqwe <ajdarzbs@gmail.com>
2023-03-01 13:38:06 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./assets/scss/main.scss";
+
+import "./assets/img/logo.svg";
+import "./assets/img/sale-background.png";
+import "./assets/img/goods-chair.png";
+import "./assets/img/subscribe-mail.svg";
+import "./assets/img/youtube.svg";
+import "./assets/img/vk.svg";
+import "./assets/img/facebook.svg";
+import "./assets/img/instagram.svg";
+
+
